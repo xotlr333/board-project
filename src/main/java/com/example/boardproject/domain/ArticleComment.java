246d07a561b736a201d79c889c0a9bd13da99d86
kongtaesic <xotlr333@naver.com>
2023-04-07 22:11:42 +0900

    #18 - 도메인 변경으로 인한 관련 소스 수정

--- src/main/java/com/example/boardproject/domain/ArticleComment.java
+++ src/main/java/com/example/boardproject/domain/ArticleComment.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.apache.catalina.User;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -14,7 +15,7 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "content"),
         @Index(columnList = "createdAt"),
@@ -27,17 +28,19 @@ public class ArticleComment extends AuditingFields{
     private Long id;
 
     @Setter @ManyToOne(optional = false) private Article article; //게시글 (ID)
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount; //유저 정보 (ID)
     @Setter @Column(nullable = false, length = 500) private String content; //본문
 
     public ArticleComment() {}
 
-    private ArticleComment(Article article, String content) {
+    private ArticleComment(UserAccount userAccount ,Article article, String content) {
+        this.userAccount = userAccount;
         this.article = article;
         this.content = content;
     }
 
-    public static ArticleComment of(Article article, String content) {
-        return new ArticleComment(article, content);
+    public static ArticleComment of(UserAccount userAccount, Article article, String content) {
+        return new ArticleComment(userAccount, article, content);
     }
 
     @Override
